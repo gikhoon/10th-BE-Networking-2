e92fb7074537e64275f50991f1e58ec89c34a7a1
gikhoon <ghikhoon@naver.com>
2024-11-19 14:03:27 +0900

    feat: 게시물 Entity 작성

--- backend/src/main/java/cotato/backend/domains/post/Post.java
+++ backend/src/main/java/cotato/backend/domains/post/Post.java
@@ -1,6 +1,10 @@
 package cotato.backend.domains.post;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +13,26 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private Long id;
 
+    @Column(name = "post_title", nullable = false)
+    private String title;
+
+    @Column(name = "post_content", nullable = false)
+    private String content;
+
+    @Column(name = "post_writer", nullable = false)
+    private String name;
+
+    @Column(name = "view", nullable = false)
+    private Long views = 0L;
+
+    public Post(String title, String content, String name) {
+        this.title = title;
+        this.content = content;
+        this.name = name;
+    }
 }
